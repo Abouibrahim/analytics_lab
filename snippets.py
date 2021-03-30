94f8f8f281c5511e066057196d3ba116c2824da9
Abouibrahim <33147155+Abouibrahim@users.noreply.github.com>
2021-03-30 16:57:32 +0300

    Update and rename snippets to snippets.py

---
+++ snippets.py
@@ -0,0 +1,7 @@
+#Configures the default settings of the page
+st.set_page_config(
+  page_title="Data Analytics Self Service App",
+  page_icon="✨",
+  layout="wide",
+  initial_sidebar_state="expanded",
+)
